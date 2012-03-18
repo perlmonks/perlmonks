b21c20e29f621e2998c2f5e4f569b9f589cf7136
Jay Bonci <jay@bonci.net>
2012-03-18 13:48:36 -0400

    Remove the e2-based url rewrite from the perlmonks drop

--- vagrant/roles/pmdev.rb
+++ vagrant/roles/pmdev.rb
@@ -1,4 +1,4 @@
 name "pmdev"
 description "Baseline squeeze development environment for everything2.com development"
 run_list("recipe[edev]")
-override_attributes "edev" => {"gitrepo" => "git://github.com/perlmonks/perlmonks.git" }
+override_attributes "edev" => {"gitrepo" => "git://github.com/perlmonks/perlmonks.git", "rewrite_urls" => false}
