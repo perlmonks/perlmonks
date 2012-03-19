e7eec649d8ea3157b0695b811c61164859a73599
Jay Bonci <jay@bonci.net>
2012-03-19 13:29:05 -0400

    New cookbooks for perlmonks

--- vagrant/roles/pmdev.rb
+++ vagrant/roles/pmdev.rb
@@ -1,4 +1,4 @@
 name "pmdev"
 description "Baseline squeeze development environment for everything2.com development"
-run_list("recipe[edev]")
+run_list("recipe[edev]","recipe[pmapp]")
 override_attributes "edev" => {"gitrepo" => "git://github.com/perlmonks/perlmonks.git", "rewrite_urls" => false}
